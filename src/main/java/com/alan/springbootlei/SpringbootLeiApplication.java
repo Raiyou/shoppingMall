ec2e2fa91e11967988024ff29ae0a839fd47c370
Lei <meteor790616@gmail.com>
2023-01-16 01:27:04 +0800

    初始化 Sprring Boot project

---
+++ src/main/java/com/alan/springbootlei/SpringbootLeiApplication.java
@@ -0,0 +1,13 @@
+package com.alan.springbootlei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLeiApplication.class, args);
+    }
+
+}
